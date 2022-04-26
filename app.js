ee28741eb9cbfd5b0d3f34b905d29d3c37849996
Kanboo <kanboowang@17.media>
2022-04-27 00:18:13 +0800

    cors add credentials

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors({ credentials: true }))
 
 app.use('/', indexRouter);
 app.use('/posts', postRouter);
