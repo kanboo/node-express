437f7c2135ad30f20295ee94b3462827037fe06a
Kanboo <kanboowang@17.media>
2022-05-01 20:12:15 +0800

    串接 Imgur API 上傳圖片

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// 串接 Imgur API 上傳圖片
+// TODO：待優化 Controller
+const { ImgurClient } = require('imgur');
+const client = new ImgurClient({ accessToken: process.env.IMGUR_ACCESS_TOKEN })
+const { successResponse, errorResponse } = require('../utils/responseHandle')
+router.post('/upload-image', async (req, res, next) => {
+  if (!req.files) {
+    errorResponse(res, 400, 'No File Selected')
+  } else {
+    var imageFile = req.files.image;
+
+    try {
+      const response = await client.upload({
+        image: Buffer.from(imageFile.data).toString('base64'),
+        type: "base64"
+      });
+
+      successResponse(res, 200, { link: response.data.link })
+    } catch (error) {
+      errorResponse(res, 400, 'Upload image fail')
+    }
+  }
 });
 
 module.exports = router;
