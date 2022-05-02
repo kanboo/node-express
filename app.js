a2da027f7440734198df1cc1d1b0ac8dcab0e9a5
Kanboo <kanboowang@17.media>
2022-05-02 11:52:21 +0800

    var 改為 const

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var session = require('express-session')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 const mongoose = require('mongoose')
 const MongoStore = require('connect-mongo');
 const fileupload = require("express-fileupload");
@@ -12,16 +12,16 @@ const passport = require('passport')
 
 require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var postRouter = require('./routes/post');
-var imageRouter = require('./routes/image');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const imageRouter = require('./routes/image');
 
 mongoose.connect(process.env.MONGODB_CONNECT)
   .then(() => console.log('Mongodb connect success'))
   .catch(e => console.error(e))
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
