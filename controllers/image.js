d741c50b36b645cd1358be1f27a18df7301ddf23
Kanboo <kanboowang@17.media>
2022-06-02 22:45:47 +0800

    新增 avatar 卡控邏輯

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,4 @@
+const sizeOf = require('image-size')
 const { ImgurClient } = require('imgur')
 const client = new ImgurClient({
   clientId: process.env.IMGUR_CLIENTID,
@@ -15,6 +16,15 @@ exports.createImage = catchAsync(async (req, res, next) => {
     errorResponse(res, 400, 'No File Selected')
   } else {
     const imageFile = req.file
+    const { type } = req.query
+
+    // 若是頭像的話，多加一個長寬比例的檢查
+    if (type === 'avatar') {
+      const dimensions = sizeOf(imageFile.buffer)
+      if (!(dimensions.width === dimensions.height && dimensions.height >= 300)) {
+        return errorResponse(res, 400, '圖片寬高比必需為 1:1 且 解析度寬度至少 300像素以上，請重新上傳')
+      }
+    }
 
     try {
       const response = await client.upload({
