b999ec7dd176afdf34296c2d925e080b3aa167d6
Kanboo <kanboowang@17.media>
2022-06-02 23:27:14 +0800

    新增 註冊成功信件通知

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,6 +13,17 @@ const { successResponse, errorResponse } = require('../utils/responseHandle')
 
 const apiErrorTypes = require('../constants/apiErrorTypes')
 
+// Mail
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: process.env.MAILER_ACCOUNT,
+    pass: process.env.MAILER_PASSWORD,
+  },
+})
+
 /**
  * 註冊
  */
@@ -37,6 +48,23 @@ const register = catchAsync(async (req, res, next) => {
     token,
     user: newUser,
   })
+
+  // 發信通知
+  transporter.sendMail({
+    from: process.env.MAILER_ACCOUNT,
+    to: email,
+    subject: 'Meta-Wall Register Success',
+    html: `
+    Congratulations on your successful registration.
+    <br>
+    <br>
+    Website: <a href="https://meta-wall.kanboo.cc/">Meta-Wall</a>
+    `,
+  })
+    .then((info) => {
+      console.log('發信通知', { info })
+    })
+    .catch(console.error)
 })
 
 /**
